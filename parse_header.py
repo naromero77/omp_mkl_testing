9b90a037ace86353c3a3b9edbec63795d22f0bd0
Kevin Gasperich <kgasperich@gmail.com>
2020-10-05 17:52:07 -0500

    fixing parser

--- parse_header.py
+++ parse_header.py
@@ -2,28 +2,78 @@
 
 import re,os
 
+def comment_remover(text):
+    '''
+    https://stackoverflow.com/a/241506
+    '''
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(pattern, replacer, text)
 
+def hash_remover(text):
+    '''
+    https://stackoverflow.com/a/241506
+    '''
+    def replacer(match):
+        s = match.group(0)
+        if (s.startswith('/') or s.startswith('#')):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"|#[^\r\n]*(?:\\\r?\n[^\r\n]*)*',
+        re.DOTALL | re.MULTILINE
+    )
+    return re.sub(r'\n\s*\n', '\n', re.sub(pattern, replacer, text),flags=re.MULTILINE)
+
+def matchall(fstr):
+    #m=re.finditer(r"(\w+)\s*(\w+)\s*\(([^;]+)\)\s*([^;]*);",fstr,re.MULTILINE)
+    #m=re.finditer(r"^(\w+)\s+(\w+)\s*\((.*?)\)",fstr,re.MULTILINE | re.DOTALL)
+    #m=re.finditer(r"\b^(?!#)([\s\w]+?)\s+(\w+)\s?\((.*?)\)",fstr,re.MULTILINE | re.DOTALL)
+    #m=re.finditer(r"^(.+?)\s(\w+)\s?\((.*?)\)",fstr,re.MULTILINE | re.DOTALL)
+    m=re.finditer(r"^([\w\s]+)\s(\w+)\s?\((.*?)\)",fstr,re.MULTILINE | re.DOTALL)
+    return m
 def hclean(fpath):
     '''
     read and clean up header file
     return list of strings (each string is a function signature)
+
+    assumes any function declaration spanning multiple lines has a comma
+      (and possibly whitespace) at the end of each broken line
     '''
     with open(fpath,'r') as f:
         d0=f.read()
+
     #put signature on one line (assume multiline signature has comma at end of each broken line)
     d0=re.sub(',\s+',',',d0)
+
     #clean up extra spaces
     #d0=re.sub(' +',' ',d0)
-    # remove blank lines and #
+
+    # split on newline
+    # remove blank lines
+    # remove lines beginning with #
     d0=list(filter(lambda x:x[0]!="#",filter(None,d0.split('\n'))))
+
     # only keep lines that end with ';'
     d0=list(filter(lambda x:x[-1]==';',d0))
     return d0
 
+# 
+# use r"(\w+)\s*(\w+)\s*\(([^;]+)\)\s*([^;]*);" to match multiline declaration (if hclean doesn't work)
+# 
 
-# use r"(\w+)\s*(\w+)\s*\(([^;]+)\)\s*([^;]*);" to parse without cleaning whitespace
-def fparse(s):
+def fparse(m):
     '''
+    TODO: fix for types with spaces (e.g. "long int", "unsigned int", etc.)
     parse string containing function signature
     returns:
     [ function_name,
@@ -34,7 +84,6 @@ def fparse(s):
       ]
     ]  
     '''
-    m=re.search(r"(\w+)\s(\w+)\s?\((.+)\)",s)
     rtype = m.group(1)
     fname = m.group(2)
     args = m.group(3).split(',')
@@ -58,15 +107,20 @@ def fparse(s):
     return (fname,rtype,argvars)
 
 if __name__ == '__main__':
-    mklh=os.path.join(os.environ['MKLROOT'],'include','mkl_blas.h')
-    #mklh='mkl_blas.h'
-    d=hclean(mklh)
-    #types = set(i.split()[0] for i in d)
-    for i,l in enumerate(d):
-        print(i)
-        print(l)
-        n,t,v = fparse(l)
-        #v = list(zip(*v))
+    #mklh=os.path.join(os.environ['MKLROOT'],'include','mkl_blas.h')
+    #mklh=os.path.join(os.environ['MKLROOT'],'include','mkl_blas_omp_offload.h')
+    #mklh=os.path.join(os.environ['MKLROOT'],'include','mkl_cblas.h')
+    mklh='../mkl_cblas.h'
+    with open(mklh,'r') as f:
+        mklstr = f.read()
+
+    m=matchall(hash_remover(mklstr))
+    for i,mi in enumerate(m):
+        print(f"\n{i}:\n{mi.group(0)}\n")
+        n,t,v = fparse(mi)
         print(n)
         print(t)
-        print(v)
+        # uncomment to transpose args
+        #v = list(zip(*v))
+        for j in v:
+            print(j)
